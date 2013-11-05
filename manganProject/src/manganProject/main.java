f83606239741c6a5cd7f12f4f13bffc90b3e97f4
RWi <s45644@beuth-hochschule.de>
2013-11-05 19:02:25 +0100

    Initial Commit

---
+++ manganProject/src/manganProject/main.java
@@ -0,0 +1,12 @@
+package manganProject;
+
+import javax.swing.JFrame;
+
+//++++++++++++++++++++++++++++++++++++++++++++++MAIN+++++++++++++++++++++++++++++++++++++++
+public class main {
+public static void main(String[] args){
+		System.out.println("Hello Project");
+		JFrame MainWindow = new JFrame();
+		MainWindow.setVisible(true);
+	}
+}
